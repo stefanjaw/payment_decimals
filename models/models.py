31217c2334007b86e263830021c9122739657767
Stefan J <stefanj@avalantec.com>
2023-05-09 00:38:29 +0000

    Updated

--- models/models.py
+++ models/models.py
@@ -4,9 +4,8 @@ from odoo import models, fields, api
 
 class AccountAbstractPayment(models.AbstractModel):
     _inherit = 'account.payment'
-    #amount = fields.Monetary(string='Payment Amount', required=True, digits=(18,7) )
     #amount = fields.Float(string='Payment Amount', digits=(18,7), required=True)
-    amount = fields.Monetary(string='Payment Amount', digits=(18,7), required=True)
+    amount = fields.Monetary(string='Payment Amount', digits=(18,7))
 
 class accountPayment(models.Model):
     _inherit = 'account.payment'
